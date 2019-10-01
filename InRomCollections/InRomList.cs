a5dd122f93e7bbbd7adbd13ced6f35e48e1e26bc
Khanh Pham <Khanh Pham@DESKTOP-6UFQOA1>
2019-10-02 00:02:21 +0700

    update

---
+++ InRomCollections/InRomList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InRomCollections
+{
+	public class InRomList<T>
+	{
+		private InRomNode _firstNode;
+		private InRomNode _lastNode;
+
+		public InRomNode First()
+		{
+			return _firstNode;
+		}
+		public InRomNode Last()
+		{
+			return _lastNode;
+		}
+	}
+}
---
+++ InRomCollections/InRomNode.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InRomCollections
+{
+	public class InRomNode
+	{
+		public string address;
+		public string nextNodeAdress;
+		public string previousNodeAddress;
+
+		public void InsertNext(InRomNode node)
+		{
+			if (nextNodeAdress == null)
+			{
+				nextNodeAdress = node.address;
+				Save();
+				return;
+			}
+
+			var nextNode = Load(nextNodeAdress);
+			node.nextNodeAdress = nextNode.address;
+			nextNodeAdress = node.address;
+			Save();
+		}
+
+		public void InsertPrevious(InRomNode node)
+		{
+			if (previousNodeAddress == null)
+			{
+				previousNodeAddress = node.address;
+				Save();
+				return;
+			}
+
+			var previousNode = Load(previousNodeAddress);
+			node.previousNodeAddress = previousNode.address;
+			previousNodeAddress = node.address;
+			Save();
+		}
+		public void Save()
+		{
+			var jsonContent = JsonConvert.SerializeObject(this);
+			File.WriteAllText(address, jsonContent);
+		}
+
+		public static InRomNode Load(string address)
+		{
+			var jsonContent = File.ReadAllText(address);
+			return JsonConvert.DeserializeObject<InRomNode>(jsonContent);
+		}
+	}
+}
